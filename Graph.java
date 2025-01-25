e0e7cd82e6dde3610a3cfd102b4f3021c8c32ca0
HenilPatel222 <henil0003patel@gmail.com>
2025-01-25 15:20:33 -0500

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -1,12 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
-/*
-Name:Henil Patel
-Student No: 7084577
-Date: 25/3/2024
 
-Assigment  3
- */
 public class Graph {
     private List<List<Integer>> adjList; // Adjacency list to represent graph
     public Graph(int vertices) {
